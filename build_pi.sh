c836d05a547b75a1a5f1a2909d17516c81db4e3c
peteuk <peteuk@users.noreply.github.com>
2015-05-03 13:34:10 +0000

    Add build_pi.sh from http://github.com/ryanfantus/piUAE

---
+++ build_pi.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# P-UAE
+#
+# 2006-2011 Mustafa TUFAN (aka GnoStiC/BRONX)
+#
+#
+#
+base=" --with-sdl --with-sdl-gl --with-sdl-gfx --with-alsa --enable-drvsnd "
+cd32=" --enable-cd32 "
+a600=" --enable-gayle "
+scsi=" --enable-scsi-device --enable-ncr --enable-a2091 "
+jit=" --enable-jit --enable-natmem "
+other=" --with-caps --enable-amax --enable-gccopt --enable-serial-port "
+debug=""
+#
+#
+./bootstrap.sh
+./configure $debug $base $cd32 $a600 $scsi $jit $other
+make
